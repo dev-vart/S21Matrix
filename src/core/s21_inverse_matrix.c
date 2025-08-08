d3ed01756288f99000f9c1df22cacda751c328dc
lubeemka <lubeemka@21-school.ru>
2025-08-08 20:00:35 +0300

    +inverse_matrix hooraygit status! on the finish line

--- src/core/s21_inverse_matrix.c
+++ src/core/s21_inverse_matrix.c
@@ -2,5 +2,19 @@
 
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
   int exit_code = OK;
+  if (!s21_is_valid_matrix(A) || !result) {
+    return INVALID_MATRIX;
+  }
+  double det = 0.0;
+  s21_determinant(A, &det);
+  if (A->rows != A->columns || det == 0.0) {
+    exit_code = CALC_ERROR;
+  } else {
+    matrix_t calc_comp_A = {0};
+    s21_calc_complements(A, &calc_comp_A);
+    s21_transpose(&calc_comp_A, result);
+    s21_remove_matrix(&calc_comp_A);
+    s21_mult_number(result, 1 / det, result);
+  }
   return exit_code;
 }
